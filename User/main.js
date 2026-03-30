010456a60f3e09b0c9af7de6683cf98b4250ca69
Dan <vfvf5389127@gmail.com>
2026-03-30 03:19:56 +0300

    Impletemented form validation and display of user info

---
+++ User/main.js
@@ -0,0 +1,36 @@
+const form = document.getElementById("profileForm");
+const clearBtn = document.getElementById("clear");
+
+const nameInput = document.getElementById("name");
+const ageInput = document.getElementById("age");
+const cityInput = document.getElementById("city");
+const hobbyInput = document.getElementById("hobby");
+
+const errorText = document.getElementById("error");
+
+const profileName = document.getElementById("profileName");
+const profileAge = document.getElementById("profileAge");
+const profileCity = document.getElementById("profileCity");
+const profileHobby = document.getElementById("profileHobby");
+
+form.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const name = nameInput.value.trim();
+    const age = ageInput.value.trim();
+    const city = cityInput.value.trim();
+    const hobby = hobbyInput.value.trim();
+
+    if (!name || !age || !city || !hobby) {
+        errorText.textContent = "Заполните все поля";
+        return;
+    }
+
+    errorText.textContent = "";
+
+    profileName.textContent = name;
+    profileAge.textContent = age;
+    profileCity.textContent = city;
+    profileHobby.textContent = hobby;
+});
